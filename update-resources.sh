97266b48920ffe4bf706cc2067222308233b44d5
Bryan Lai <bryanlais@gmail.com>
2019-08-10 00:04:07 +0800

    [p13n] Update mathjax scripts

--- update-resources.sh
+++ update-resources.sh
@@ -3,8 +3,9 @@
 
 # LaTeX
 LATEX_MACROS_UPSTREAM=../Base/Templates/LaTeX/basics
+LATEX_MACROS_LOCAL=./_includes/usermod
 for tex_file in "$LATEX_MACROS_UPSTREAM"/*.tex; do
-    local_file=./_includes/usermod/"$(basename "$tex_file")"
+    local_file="$LATEX_MACROS_LOCAL"/"$(basename "$tex_file")"
     [[ ! -f $local_file ]] && continue
     if diff --color "$local_file" "$tex_file";
     then :
@@ -12,3 +13,13 @@ for tex_file in "$LATEX_MACROS_UPSTREAM"/*.tex; do
         cp -i "$tex_file" "$local_file"
     fi
 done
+
+tex_cmds=$(cat "$LATEX_MACROS_LOCAL"/*.tex)
+cat <<- EOF > "$LATEX_MACROS_LOCAL"/mathjax.html
+	<!-- mathjax for local use -->
+	<!-- for deployment, see '_includes/main/top/custom.html' -->
+
+	<div class="mathjax_macros" style="display: none;">
+	\$ $tex_cmds \$
+	</div>
+EOF
